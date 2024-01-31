e14a2416ffe4d191ef89f29bff88f3e117fc73a1
DaniyarAbS <120436857+DaniarAbS@users.noreply.github.com>
2024-01-31 14:52:19 +0600

    Adding the file client to repository

---
+++ client/src/base/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+    //Font colors
+    white: '#FFFFFF',
+    black: '#000000',
+    
+  };
+  
---
+++ client/src/base/sizes.js
@@ -0,0 +1,9 @@
+export const sizes = {
+    // Font sizes
+    small: '12px',
+    medium: '16px',
+    large: '20px',
+    xLarge: '24px',
+    xxLarge: '32px',
+  };
+  
